49787d384aef6608e69632a3f3617ee92ab084a7
Rya Meyvriska <rya.meyvriska@mail.ugm.ac.id>
2017-05-28 12:15:58 +0700

    Ubah key_callback jadi 1 merubah grid dengan batas pixelX dan pixelY yang sesuai merubah ukuran window dan nama window jadi Super Usus

--- main.cpp
+++ main.cpp
@@ -1,17 +1,14 @@
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
 #include <math.h>
 #include <windows.h>
+using namespace std;
 float x=0;
 bool play = false;
 bool gameOver = false;
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE); // close program on ESC key
-}
-
+int pixelX = 11;
+int pixelY = 16;
 void processNormalKeys(GLFWwindow* window, int key, int scancode, int action,int mods) {
 	if(key==GLFW_KEY_ENTER && action == GLFW_PRESS){
             play = true;
@@ -28,12 +25,14 @@ void processNormalKeys(GLFWwindow* window, int key, int scancode, int action,int
 			gameOver = true;
 		else
 			x+=1;
+	}else if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, GL_TRUE);
 	}
 }
 
 void setup_viewport(GLFWwindow* window)
 {
-   float ratio;
+    float ratio;
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     ratio = width / (float) height;
@@ -42,34 +41,35 @@ void setup_viewport(GLFWwindow* window)
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-10, 10, -10, 10, 1000.f, -1000.f);
+    glOrtho(-pixelX, pixelX, -pixelY, pixelY, 1000.f, -1000.f);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
 
 void grid(){
+    //vertikal
 	glBegin(GL_LINES);
-	for(int i=0;i<=20;i++){
-		if(-10+i==0)
+	for(int i=0;i<=2*pixelX;i++){
+		if(-pixelX+i==0)
 			glColor3ub(192,192,192);
 		else
 			glColor3ub(128,128,128);
-		glVertex2f(-10+i,10);
-		glVertex2f(-10+i,-10);
+		glVertex2f(-pixelX+i,pixelY);
+		glVertex2f(-pixelX+i,-pixelY);
 	}
 	glEnd();
 
+	//horizontal
 	glBegin(GL_LINES);
-	for(int i=0;i<=20;i++){
-		if(-10+i==0)
+	for(int i=0;i<=2*pixelY;i++){
+		if(-pixelY+i==0)
 			glColor3ub(192,192,192);
 		else
 			glColor3ub(128,128,128);
-		glVertex2f(10,-10+i);
-		glVertex2f(-10,-10+i);
+		glVertex2f(pixelX,-pixelY+i);
+		glVertex2f(-pixelX,-pixelY+i);
 	}
 	glEnd();
-
 }
 
 void rectangle(float p, float l){
@@ -130,7 +130,7 @@ int main(void)
     GLFWwindow* window;
     if (!glfwInit()) exit(EXIT_FAILURE);
 
-    window = glfwCreateWindow(600, 600, "Game Mini", NULL, NULL);
+    window = glfwCreateWindow(529, 732, "Super Usus", NULL, NULL);
 
     if (!window)
     {
@@ -140,7 +140,6 @@ int main(void)
 
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
-    glfwSetKeyCallback(window, key_callback);
     glfwSetKeyCallback(window,processNormalKeys);
 
     while (!glfwWindowShouldClose(window))
